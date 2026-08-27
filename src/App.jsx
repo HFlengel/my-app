dd5bf854fd1cac979864653908e047040128406d
HFlengel <henrikflengel@gmail.com>
2026-08-27 10:59:06 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import './App.css'
+
+const SAMPLE_TASKS = ["Buy milk", "Call the landlord", "Book the dentist",
+                      "Water the plants", "Reply to Mette"];
+
+function randomTask() {
+  return SAMPLE_TASKS[Math.floor(Math.random() * SAMPLE_TASKS.length)];
+}
+
+function TodoItem({ text }) {
+  return <li className="todo-item">{text}</li>;
+}
+
+export default function TodoList() {
+  const [todos, setTodos] = useState([randomTask()]);
+
+  function handleAdd() {
+    setTodos([...todos, randomTask()]);
+  }
+
+  return (
+    <>
+      <h1>My To-Do ({todos.length})</h1>
+      <ul>
+        {todos.map((text, index) => (
+          <TodoItem key={index} text={text} />
+        ))}
+      </ul>
+      <button onClick={handleAdd}>Add</button>
+    </>
+  );
+}
