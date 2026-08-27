131e910193d6b3292fed8c4896c349c40a3fab77
HFlengel <henrikflengel@gmail.com>
2026-08-27 11:40:26 +0200

    No repeat tasks

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,15 @@ import './App.css'
 
 const SAMPLE_TASKS = ["Buy milk", "Call the landlord", "Book the dentist",
                       "Water the plants", "Reply to Mette"];
+let REMAINING_TASKS = SAMPLE_TASKS;
 
 function randomTask() {
-  return SAMPLE_TASKS[Math.floor(Math.random() * SAMPLE_TASKS.length)];
+  let i = Math.floor(Math.random() * REMAINING_TASKS.length);
+  let chosenTask = REMAINING_TASKS[i];
+
+  REMAINING_TASKS.splice(i, 1); // Remove task from pool
+
+  return chosenTask;
 }
 
 function TodoItem({ text }) {
@@ -13,21 +19,22 @@ function TodoItem({ text }) {
 }
 
 export default function TodoList() {
-  const [todos, setTodos] = useState([randomTask()]); // List now starts random
+  const [todos, setTodos] = useState([]);
 
   function handleAdd() {
+    if (REMAINING_TASKS.length == 0) { alert("No More Tasks"); return;} // Prevents repeat tasks
     setTodos([...todos, randomTask()]);
   }
 
   return (
     <>
       <h1>My To-Do ({todos.length})</h1>
+      <button onClick={handleAdd}>Add</button>
       <ul>
         {todos.map((text, index) => (
           <TodoItem key={index} text={text} />
         ))}
       </ul>
-      <button onClick={handleAdd}>Add</button>
     </>
   );
 }
